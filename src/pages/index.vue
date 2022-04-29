da9228a8a749ac270f10b53b0bdc6ee17d344f83
enoshima7 <duanjipeng0328@gmail.com>
2022-04-29 11:27:56 +0800

    feat: init

---
+++ src/pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+function formatIndex(index: number) {
+  if (index < 10)
+    return `0${index}`
+  return index
+}
+</script>
+
+<template>
+  <ul>
+    <li v-for="item in 10" :key="item" font-mono text-2xl py-2>
+      <span hover-opacity-80 cursor-pointer>
+        <span italic>{{ formatIndex(item) }}</span> <span>算法</span>
+      </span>
+    </li>
+  </ul>
+</template>
+<style scoped>
+</style>
